c62f29be11ae18229cce60cbe68dcd7fbab2ae5f
ndaDayo <hayato711+nda@gmail.com>
2022-12-02 21:36:58 +0900

    wip test

---
+++ monkey/src/monkey/lexer/lexer_test.go
@@ -0,0 +1,28 @@
+package lexer
+
+import (
+    "testing"
+    "monkey/token"
+)
+
+func TestNextToken(t *testing.T){
+    input := `=+(){},;`
+
+    tests := []struct{
+        expectedType  token.TokenType
+        expectedLiteral string
+    }{
+        {token.ASSIGN, "="},
+    }
+
+    l := New(input)
+
+    for i, tt := range tests{
+        tok := l.NextToken()
+
+        if tok.Type != tt.expectedType {
+            t.Fatalf("tests[%d] - tokentype wring. expected=%q, got=%q",
+        i, tt.expectedType, tok.Type)
+        }
+    }
+}
