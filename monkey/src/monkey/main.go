0b1b54ad641feb440ed38fd5f0ed02e7fbe46dc9
ndaDayo <hayato711+nda@gmail.com>
2022-12-04 16:26:36 +0900

    [add] repl and main

---
+++ monkey/src/monkey/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programing language!\n",
+		user.Username)
+	fmt.Printf("Feel free type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
