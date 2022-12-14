2c1c3e3fd839d2f66d0ffdae957748161cb0c22f
ndaDayo <hayato711+nda@gmail.com>
2022-12-14 17:03:24 +0900

    add test boolean

--- monkey/src/monkey/parser/parser.go
+++ monkey/src/monkey/parser/parser.go
@@ -62,6 +62,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	p.nextToken()
 	p.nextToken()
@@ -274,4 +276,8 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	expression.Right = p.parseExpression(precedence)
 
 	return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
