cd887dc15f456408f1e1d0de12fa24f4fe99f299
ndaDayo <hayato711+nda@gmail.com>
2022-12-22 21:01:04 +0900

    fix test assertion

--- monkey/src/monkey/evaluator/evaluator_test.go
+++ monkey/src/monkey/evaluator/evaluator_test.go
@@ -319,13 +319,13 @@ func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
 
 	evaluated := testEval(input)
-
 	str, ok := evaluated.(*object.String)
 
 	if !ok {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
-	if str.Value != "Hello World" {
+
+	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
